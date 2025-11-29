53a3d5c9883a56c3ba2e83af7f0f4312e549c537
tarunmittal2007 <tarunmittal8572@gmail.com>
2025-11-29 16:55:46 +0530

    Create program138.c

---
+++ program138.c
@@ -0,0 +1,24 @@
+/*Q138: Print all enum names and integer values using a loop.
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+RED=0
+YELLOW=1
+GREEN=2
+*/
+
+#include<stdio.h>
+enum Color { RED, YELLOW, GREEN };
+int main() 
+{
+enum Color c;
+const char *names[] = { "RED","YELLOW","GREEN" };
+
+for (c = RED ; c <= GREEN ; c++) 
+{
+printf("%s=%d\n", names[c], c);
+}
+
+return 0;
+}
